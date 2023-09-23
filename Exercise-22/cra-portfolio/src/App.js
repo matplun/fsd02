c19a15256d87577f00eca74bee7f55f006f30949
ripspace <matplun@gmail.com>
2023-09-22 21:57:51 -0700

    Started Exercises 21-23

---
+++ Exercise-22/cra-portfolio/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+// We now know how to create a ReactJS project using CRA.
+
+// Recreate the portfolio page using FunctionalComponents, routes in ReactJS.
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Exercise-23/cra-cardsapp/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+// We now know how to create a ReactJS project without having to worry about 
+// setting up the environment.
+
+// Create an IDCards App using FunctionalComponents, using CRA.
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
